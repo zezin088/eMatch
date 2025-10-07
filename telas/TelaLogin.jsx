7e4fda046df1bac29094e85c246b036f5287a6b6
anabia278 <ana.teixeira127@etec.sp.gov.br>
2025-10-07 20:34:36 -0300

    Adicionando botao

--- telas/TelaLogin.jsx
+++ telas/TelaLogin.jsx
@@ -1,9 +1,12 @@
-import{View, Text} from 'react-native';
+import{View, Text, Button} from 'react-native';
 
-function TelaLogin() {
+function TelaLogin({ navigation}) {
     return (
         <view>
             <text>Tela Login</text>
+
+            <Button title='Ir para Cadastro' onPress={()=> navigation.navigate('Cadastro')}/>
+            <Button title='Ir para Descobrir' onPress={()=> navigation.navigate('Descobrir')}/>
         </view>
     )
 }
